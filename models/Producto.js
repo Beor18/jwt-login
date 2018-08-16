5bee627a568106aa072f4581198adffaf22e2e73
Fernando Lopez <fer.ragnar19@gmail.com>
2018-08-15 21:36:16 -0300

    agregado modelo Productos y nuevo archivo rutas seguras

---
+++ models/Producto.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ProductoSchema = new Schema({
+    titulo: { type: String },
+    autor: { type: String }
+
+});
+
+const Producto = mongoose.model('productos', ProductoSchema);
+
+module.exports = Producto;
