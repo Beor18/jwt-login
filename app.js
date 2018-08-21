7d1792a9975d6b0fb70bdd2197392f45e6f59ac7
Fernando Lopez <fer.ragnar19@gmail.com>
2018-08-21 09:27:55 -0300

    solucionado cors

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 );
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
 app.use(passport.initialize());
 require('./passport')(passport);
 
