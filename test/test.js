0b635021be9ced8720935a4c5a2fa0478f3209c4
Beor18 <fer.ragnar19@gmail.com>
2018-10-09 20:24:56 -0300

    agregado testing

---
+++ test/test.js
@@ -0,0 +1,20 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+const url = 'http://localhost:5000';
+
+// Testeando api rest: registrar usuario
+describe('/ POST /api/users/login', () => {
+    it('Se ha registrado un usuario', (done) => {
+        chai.request(url)
+            .post('/api/users/register')
+            .send({ id: 0, name: "Prueba", email: "testing@demo.com", password: "12345678", password_confirm: "12345678" })
+            .end(function(err, res) {
+                console.log(res.body)
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+});
