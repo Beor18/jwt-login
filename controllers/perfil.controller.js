5111707301947468399b4dc337d84b043a018320
Beor18 <fer.ragnar19@gmail.com>
2018-10-18 10:57:22 -0300

    unos pequeños arreglos

--- controllers/perfil.controller.js
+++ controllers/perfil.controller.js
@@ -1,17 +1,23 @@
 const User = require('../models/User');
 const gravatar = require('gravatar');
 const passport = require('passport');
+const { getLogger } = require('@jwt/utils')
+const log = getLogger(__dirname, __filename)
 
 async function getPerfil(req, res, next) {
-    let token = req.query.secret_token;
-    return res.json({
-        id: req.user.id,
-        name: req.user.name,
-        avatar: req.user.avatar,
-        email: req.user.email,
-        token: req.query.secret_token,
-        productos_url: '/api/productos?secret_token=' + token
-    });
+    try {
+        let token = req.query.secret_token;
+        return res.json({
+            id: req.user.id,
+            name: req.user.name,
+            avatar: req.user.avatar,
+            email: req.user.email,
+            token: req.query.secret_token,
+            productos_url: '/api/productos?secret_token=' + token
+        });
+    } catch (err) {
+        log.error('Ups Hubo un error! ' + err);
+    }
 }
 
 module.exports = {
